fa11acd9defb8561b2cb6a787c8c939a71a7e724
zapalap <przemyslaw.zapala@gmail.com>
2015-11-07 12:35:36 +0100

    Add postgres trigger and trigger function to solution

---
+++ HushDomain/Db/update_geom.sql
@@ -0,0 +1,19 @@
+﻿CREATE OR REPLACE FUNCTION update_message_geom() 
+RETURNS TRIGGER 
+AS $$
+DECLARE
+	latitude double precision;
+	longitude double precision;
+BEGIN
+latitude := NEW.lat;
+longitude := NEW.long;
+
+  NEW.geom := ST_GeomFromText('POINT(' || longitude || ' ' || latitude || ')', 4326);
+RETURN NEW;
+END $$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER update_message_geom_trigger
+BEFORE INSERT OR UPDATE ON messages
+FOR EACH ROW 
+EXECUTE PROCEDURE update_message_geom();
