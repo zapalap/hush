c9b3f9f4025b610f9720f176d9838c3c87d9f8cb
zapalap <przemyslaw.zapala@gmail.com>
2015-11-07 22:37:34 +0100

    Ensure json is serialized using camel case

--- HushApi/Global.asax.cs
+++ HushApi/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,14 @@ namespace HushApi
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            GlobalConfiguration
+                .Configuration
+                .Formatters
+                .JsonFormatter
+                .SerializerSettings
+                .ContractResolver = new CamelCasePropertyNamesContractResolver();
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
